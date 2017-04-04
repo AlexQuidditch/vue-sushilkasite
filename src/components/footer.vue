dd00aee488ab5d1dcc4cfaa09bf6b5fb8762cf0f
AlexQuidditch <alexquidditch@gmail.com>
2017-04-05 02:36:58 +0700

    created new sections

---
+++ src/components/footer.vue
@@ -0,0 +1,216 @@
+<template lang="html">
+	<footer id="footer" class="footer">
+
+		<div class="container_flex-row">
+
+			<div class="social">
+				<span class="social__text">{{ social.name }}</span>
+				<ul class="social__list">
+					<li v-for="contentItem in social.content" class="social__item">
+						<a
+							:href="contentItem.link"
+							class="social__link"
+							ripple-light
+							>
+							<i :class=" 'fa fa-' + contentItem.icon "
+								aria-hidden="true"
+								>
+							</i>
+						</a>
+					</li>
+				</ul>
+			</div>
+
+			<div class="feedback">
+				<a
+					:href="feedback.link"
+					class="feedback__button"
+					ripple-light
+					><i class="fa fa-envelope-o" aria-hidden="true"></i> &nbsp;{{ feedback.text }}</a>
+			</div>
+
+			<div class="requisites">
+
+				<div class="requisites__column">
+					<span class="requisites__name">{{ official.name }}</span>
+					<ul class="requisites__list">
+						<li v-for="contentItem in official.content" class="requisites__item">{{ contentItem }}</li>
+					</ul>
+				</div>
+
+				<div class="requisites__column">
+					<span class="requisites__name">{{ address.name }}</span>
+					<ul class="requisites__list">
+						<li v-for="contentItem in address.content" class="requisites__item">{{ contentItem }}</li>
+					</ul>
+				</div>
+
+				<div class="requisites__column">
+					<span class="requisites__name">{{ phones.name }}</span>
+					<ul class="requisites__list">
+						<li v-for="contentItem in phones.content" class="requisites__item">{{ contentItem }}</li>
+					</ul>
+				</div>
+
+				<div class="requisites__column">
+					<span class="requisites__name">{{ mail.name }}</span>
+					<ul class="requisites__list">
+						<li v-for="contentItem in mail.content" class="requisites__item">
+							<a
+								class="requisites__mail"
+								:href=" 'mailto:' + contentItem.text"
+								:title="contentItem.title"
+								>{{ contentItem.text }}</a>
+						</li>
+					</ul>
+				</div>
+
+			</div>
+
+		</div>
+
+	</footer>
+</template>
+
+<script>
+	export default {
+		name: "footer",
+		data() {
+			return {
+				social: {
+					name: 'Мы в соцсетях:',
+					content: [
+						{
+							icon: 'vk',
+							link: '//суши-ачинск.рф'
+						}
+					]
+				},
+				feedback: {
+					link: 'mailto:Vaz211483@mail.ru',
+					text: 'Отправить отзыв'
+				},
+				official: {
+					name: 'ООО "Дуэт"',
+					content: [
+						'ИНН 2443045445',
+						'ОГРН 1152443000456'
+					]
+				},
+				address: {
+					name: 'Адрес:',
+					content: [
+						'г. Ачинск, мкр-он 4, д.29, пом.57'
+					]
+				},
+				phones: {
+					name: 'Телефоны:',
+					content: [
+						'+7 (953) 849-39-30',
+						'+7 (962) 071-52-22',
+						'93-93-0'
+					]
+				},
+				mail: {
+					name: 'Почта:',
+					content: [
+						{
+							text: 'Vaz211483@mail.ru',
+							title: 'Напишите нам :)'
+						}
+					]
+				}
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+
+	@import '../scss/partials/_layout';
+	@import '../scss/partials/_mixins';
+	@import '../scss/partials/_variables';
+
+	.footer {
+		max-width: 1180px;
+		margin: auto;
+		padding: 5vh 1vh;
+		background-color: #efdecd;
+		@include MDShadow-1;
+	}
+
+	.footer .container_flex-row {
+		padding: 30px;
+		background-color: $main-elm;
+		@include MDShadow-1;
+	}
+
+	.social {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex: 1 1 49%;
+		&__text {
+			display: inline-block;
+			vertical-align: middle;
+		}
+		&__list {
+			display: inline-block;
+			width: 50%;
+			text-align: center;
+			vertical-align: middle;
+		}
+		&__item {
+			display: inline-block;
+		}
+		&__link {
+			padding: 8px 8px 10px;
+			font-size: 2rem;
+			color: $main-elm #5d80a6;
+			transition: box-shadow 0.3s ease-in-out;
+			@include MDShadow-1;
+			&:hover {
+				@include MDShadow-2;
+			}
+			&:visited {
+				color: $main-elm;
+			}
+		}
+	}
+
+	.feedback {
+		flex: 1 1 49%;
+		text-align: center;
+		padding: 10px;
+		&__button {
+			display: inline-block;
+			width: 50%;
+			padding: 10px;
+			font-size: 1.4rem;
+			color: $main-elm $second-elm;
+			border: none;
+			transition: box-shadow 0.3s ease-in-out;
+			@include MDShadow-1;
+			&:hover {
+				@include MDShadow-2;
+			}
+		}
+	}
+
+	.requisites {
+		display: flex;
+		flex: 1 1 100%;
+		flex-wrap: wrap;
+		justify-content: space-between;
+		align-items: flex-start;
+		padding-top: 3vh;
+		&__column {
+			flex: 1 1 25%;
+			text-align: center;
+		}
+		&__name {
+			font-size: 1.2rem;
+			line-height: 1.5em;
+		}
+	}
+</style>
---
+++ src/components/information.vue
@@ -0,0 +1,143 @@
+<template lang="html">
+	<section id="information" class="information">
+
+		<div class="container_flex-row">
+
+			<h2 class="information-title">Наши постоянные акции:<br>
+				<span class="information-title__sub">Обязательно ознакомьтесь!</span>
+			</h2>
+
+			<ul class="information-list">
+				<li v-for="informationItem in informationList" class="information-list__card">
+					<h6 class="information-list__title">{{ informationItem.title }}</h6>
+					<component :is = "informationItem.icon" class = "information-list__icon"></component>
+					<span class="information-list__text">{{ informationItem.text }}</span>
+				</li>
+			</ul>
+
+			<span class="information__additional"></span>
+
+		</div>
+	</section>
+</template>
+
+<script>
+import Birthday from './icons/birthday';
+import Gift from './icons/gift';
+import Rushhour from './icons/rushhour';
+import Delivery from './icons/delivery';
+
+export default {
+	name: "information",
+	data() {
+		return {
+			informationList: [{
+					title: 'Скидка имениннику!',
+					icon: 'Birthday',
+					text: 'В день рождения дарим скидку 15%! Скидка действует в течение 5 дней: за день до, в день рождения и три дня после него.'
+				},
+				{
+					title: 'Горячий час!',
+					icon: 'Rushhour',
+					text: 'С понедельника по пятницу с 12:00 до 14:00 сделай заказ и получи скидку 10%*'
+				},
+				{
+					title: 'Подарок!',
+					icon: 'Gift',
+					text: 'При заказе от 1500 руб. в подарок игристый напиток!'
+				},
+				{
+					title: 'Бесплатная доставка!',
+					icon: 'Delivery',
+					text: 'При \n заказе от 500 рублей доставка по городу бесплатно!'
+				},
+			],
+			additional: '* Акция не действует в выходные и праздничные дни.'
+		}
+	},
+	components: {
+		Birthday,
+		Gift,
+		Rushhour,
+		Delivery
+	}
+}
+</script>
+
+<style lang="scss">
+
+@import '../scss/partials/_layout';
+@import '../scss/partials/_mixins';
+@import '../scss/partials/_variables';
+
+$icon-size: 180px;
+
+.information-title {
+    text-align: center;
+    width: 100%;
+    font-size: 3em;
+}
+.information-list {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: center;
+    align-content: center;
+    align-self: center;
+    margin: 5% auto 1%;
+    &__card {
+        display: flex;
+        flex: 1 1 auto;
+        flex-flow: column wrap;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        width: 31%;
+        padding: 20px;
+        margin: 10px;
+        background-color: $main-elm;
+        cursor: default;
+        transition: box-shadow 0.3s ease-in-out;
+        @include MDShadow-1;
+        &:hover {
+            @include MDShadow-2
+        }
+    }
+    &__title {
+        font-size: 1.5rem;
+		color: $second-elm;
+    }
+    &__icon {
+        width: $icon-size;
+        height: $icon-size;
+        margin: 10px;
+        path {
+            size: 100%;
+            fill: rgba($second-elm, .9);
+            .information-list__card:hover & {
+                animation: 1.2s ease-in-out 0s normal none infinite running trambling-animation;
+            }
+            .information-list__card:last-child:hover & {
+                animation: none;
+            }
+        }
+    }
+    &__text {
+        text-align: center;
+    }
+}
+@keyframes trambling-animation {
+    0%,
+    100%,
+    50% {
+        transform: translateX(0);
+    }
+    10%,
+    30% {
+        transform: translateX(-2.5%);
+    }
+    20%,
+    40% {
+        transform: translateX(2.5%);
+    }
+}
+</style>
